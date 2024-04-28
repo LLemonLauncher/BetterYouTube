3beecdb760be94cbb3d0f89934fb7b25963c684b
Joshi <joshua@lipps.de>
2024-04-28 22:33:12 +0200

    content script ig

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
- console.log("Roflkopter");
+console.log("Roflkopter");
 //import * as content_scripts from "./content_scripts.js";
 
- 
- if (localStorage.getItem('blockList') == null && localStorage.getItem('windowOpen') == null) {
-    let list = new Array();
-    localStorage.setItem('blockList', JSON.stringify(list));
-    localStorage.setItem('windowOpen', false);
-    console.log("Local storage created");
- }
- 
-  
+
+if (localStorage.getItem('blockList') == null && localStorage.getItem('windowOpen') == null) {
+  let list = new Array();
+  localStorage.setItem('blockList', JSON.stringify(list));
+  localStorage.setItem('windowOpen', false);
+  console.log("Local storage created");
+}
+
+
 window.addEventListener('DOMContentLoaded', function() {
-  
+
   //searchBlockTerms();
   var storedList = localStorage.getItem('blockList');
   if (storedList) {
@@ -49,8 +49,8 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 });
 
 
-function showList() {	
-  
+function showList() {
+
   list.sort();
   checkboxContainer.innerHTML = "";
 
@@ -61,11 +61,11 @@ function showList() {
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = `checkbox${index}`;
-    
+
     const label = document.createElement("label");
     label.htmlFor = `checkbox${index}`;
     label.appendChild(document.createTextNode(element));
-    
+
     checkboxContainer.appendChild(checkbox);
     checkboxContainer.appendChild(label);
     checkboxContainer.appendChild(document.createElement("br"));
@@ -78,7 +78,7 @@ const checkboxContainer = document.getElementById("checkbox-container");
 
 submitButton.addEventListener("click", function() {
 
-  if(userInput.value !== "" && list.indexOf(userInput.value) === -1){
+  if (userInput.value !== "" && list.indexOf(userInput.value) === -1) {
     list.push(userInput.value.trim().toLowerCase());
     showList();
     return;
@@ -86,43 +86,41 @@ submitButton.addEventListener("click", function() {
 
   let checkboxes = checkboxContainer.querySelectorAll('input[type="checkbox"]');
   checkboxes.forEach(checkbox => {
-    if(checkbox.checked){
-      console.log("Checkbox checked: "+"checkbox.value");
+    if (checkbox.checked) {
+      console.log("Checkbox checked: " + "checkbox.value");
       list.splice(list.indexOf(checkbox.nextElementSibling.textContent), 1)
     }
   });
   showList();
   searchBlockTerms();
-  
+
 })
 
 function searchBlockTerms() {
 
   console.log("searchBlockTerms is being executed");
 
-    //const videoTitles = document.querySelectorAll('.ry-badge ry-badge-dark');
+  //const videoTitles = document.querySelectorAll('.ry-badge ry-badge-dark');
 
-    const videoTitles = [...document.querySelectorAll('yt-formatted-string.style-scope ytd-rich-grid-media')].map(x => x.innerText);
+  const videoTitles = [...document.querySelectorAll('#video-title.style-scope.ytd-rich-grid-media')].map(x => x.innerText);
 
-    //const videoTitles = document.getElementById('video-title');
+  //const videoTitles = document.getElementById('video-title');
 
-    //const videoTitles = document.querySelectorAll('yt-logo-updated-svg_yt2');
-    console.log(videoTitles);
-  
-    videoTitles.forEach(titleElement => {
-      const titleText = titleElement.textContent.toLowerCase(); // Get the lowercase text content of the title
-      const foundWords = list.filter(word => titleText.includes(word.toLowerCase()));
-    
-      if (foundWords.length > 0) {
-        console.log(`Title: ${titleText}`);
-        console.log(`Found words: ${foundWords.join(", ")}`);
-      } else {
-        console.log("No words found, sadge :(");
-      }
-    });
-  }
+  //const videoTitles = document.querySelectorAll('yt-logo-updated-svg_yt2');
+  console.log(videoTitles);
 
+  videoTitles.forEach(titleElement => {
+    const titleText = titleElement.textContent.toLowerCase(); // Get the lowercase text content of the title
+    const foundWords = list.filter(word => titleText.includes(word.toLowerCase()));
 
+    if (foundWords.length > 0) {
+      console.log(`Title: ${titleText}`);
+      console.log(`Found words: ${foundWords.join(", ")}`);
+    } else {
+      console.log("No words found, sadge :(");
+    }
+  });
+}
 
 
 
@@ -130,6 +128,8 @@ function searchBlockTerms() {
 
 
 
-  
 
-  
+
+
+
+
--- content_scripts.js
+++ content_scripts.js
@@ -2,22 +2,25 @@ function searchBlockTerms() {
 
   console.log("searchBlockTerms is being executed");
 
-    const videoTitles = [...document.querySelectorAll('yt-formatted-string.style-scope ytd-rich-grid-media')].map(x => x.innerText);
+  const videoTitles = [...document.querySelectorAll('#video-title.style-scope.ytd-rich-grid-media')].map(x => x.innerText);
 
-    //const videoTitles = document.getElementById('video-title');
+  //const videoTitles = document.getElementById('video-title');
 
-    //const videoTitles = document.querySelectorAll('yt-logo-updated-svg_yt2');
-    console.log(videoTitles);
-  
-    videoTitles.forEach(titleElement => {
-      const titleText = titleElement.textContent.toLowerCase(); // Get the lowercase text content of the title
-      const foundWords = list.filter(word => titleText.includes(word.toLowerCase()));
-    
-      if (foundWords.length > 0) {
-        console.log(`Title: ${titleText}`);
-        console.log(`Found words: ${foundWords.join(", ")}`);
-      } else {
-        console.log("No words found, sadge :(");
-      }
-    });
-  }
+  //const videoTitles = document.querySelectorAll('yt-logo-updated-svg_yt2');
+  console.log(videoTitles);
+  console.log(document);
+
+  videoTitles.forEach(titleElement => {
+    const titleText = titleElement.textContent.toLowerCase(); // Get the lowercase text content of the title
+    const foundWords = list.filter(word => titleText.includes(word.toLowerCase()));
+
+    if (foundWords.length > 0) {
+      console.log(`Title: ${titleText}`);
+      console.log(`Found words: ${foundWords.join(", ")}`);
+    } else {
+      console.log("No words found, sadge :(");
+    }
+  });
+}
+
+searchBlockTerms();
