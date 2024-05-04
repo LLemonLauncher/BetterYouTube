8768401078e9c28055ed6f31199f15ec4467af50
Joshi <joshua@lipps.de>
2024-05-04 21:15:02 +0200

    'not interested' worked once

--- content_scripts.js
+++ content_scripts.js
@@ -10,40 +10,44 @@ function searchBlockTerms() {
   console.log(videoTitles);
 
   browser.runtime.sendMessage({
-    message : "lol"
+    message: "lol"
   }).then((message) => {
-    videoTitles.forEach(titleElement => {
+    let timer_index = 1;
+    videoTitles.forEach((titleElement, index) => {
       const titleText = titleElement.toLowerCase(); // Get the lowercase text content of the title
-      
+
       console.log(message);
       const list = message ? JSON.parse(message) : [];
 
       const foundWords = list.filter(word => titleText.includes(word.toLowerCase()));
-    
+      const video_selector = "div#menu.style-scope.ytd-rich-grid-media>ytd-menu-renderer.style-scope.ytd-rich-grid-media>yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer";
+      const menubutton_selector = "tp-yt-paper-item.style-scope.ytd-menu-service-item-renderer";
+
       if (foundWords.length > 0) {
         console.log(`Title: ${titleText}`);
         console.log(`Found words: ${foundWords.join(", ")}`);
+        console.log(`Index: ${index} => Index / 2: ${index / 2}`);
 
-        const notInterestedButton = document.querySelector('#button.style-scope.yt-icon-button');
-        //const notInterestedOption = document.querySelector('paper-listbox.style-scope.ytd-menu-popup-renderer li:nth-child(2)');
-        if (notInterestedButton) {
-          notInterestedButton.click();
+        setTimeout(() => {
+          let video = document.querySelectorAll(video_selector);
+          console.log(video);
+          // video[index / 2].children[0].click();
 
-          console.log("Button clicked");
-          } 
-        }
-        else {
-          console.log("No Not Interested button found sadge :(");
-        }
-      
+          console.log("selected video menu");
+          setTimeout(() => {
+            document.querySelectorAll(menubutton_selector)[4].click();
+            console.log("removed video");
+          }, 400);
+        }, 500 * (timer_index++));
+      }
     });
   });
 }
 
-  setTimeout(function() {
-    console.log("searchBlockTerms is being executed blankkkk");
-    searchBlockTerms();
-  }, 1000);
+setTimeout(function() {
+  console.log("searchBlockTerms is being executed blankkkk");
+  searchBlockTerms();
+}, 1000);
 
-  
+
 
